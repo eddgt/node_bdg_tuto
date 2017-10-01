ae66903ba986e6689bd8c3a62e3854af141e63b5
TAmyG <tamy.vivas@gmail.com>
2017-10-01 16:47:20 -0600

    type: feat, delete implementado

--- controllers/tvController.js
+++ controllers/tvController.js
@@ -15,11 +15,15 @@ const tvShows = [
 
     },
 ]
-
+/**
+ * Obtener todos los tv shows
+ */
 controller.getTV = (req, res, next)=> {
     res.send(tvShows);
 }
-
+/**
+ * Obtener tv show por ID
+ */
 controller.getTVById = (req, res, next)=>{
     let tvFind  = tvShows.find(x => x.Id === parseInt(req.params.id));
 
@@ -28,49 +32,53 @@ controller.getTVById = (req, res, next)=>{
     }
     res.send(tvFind);
 }
-
+/**
+ * Agregar tv show
+ */
 controller.addTV = (req, res, next)=>{
     let id = tvShows.length + 1;
-    let newTVShow = {
-        Id: id,
-        titulo: req.body.titulo,
-        anio: req.body.anio,
-        pais: req.body.pais
-    };
+    let newTVShow = buildTVShow(id, req.body);
 
     tvShows.push(newTVShow);
     res.send(newTVShow);
 }
-
+/**
+ * Actualizar tv show
+ */
 controller.updateTV = (req, res, next)=>{
     let tvShow = {error: 'tv show no econtrado'};
     //Forma 1
 
-    /*let tvIndex  = tvShows.findIndex(x => x.Id === parseInt(req.params.id));
+    let tvIndex  = tvShows.findIndex(x => x.Id === parseInt(req.params.id));
     
     if(tvIndex < 0){
         return res.send(tvShow);
     }
     
     tvShow = tvShows[tvIndex];
-    tvShow.titulo = req.body.titulo; 
-    tvShow.anio = req.body.anio;
-    tvShow.pais = req.body.pais;
+    tvShow = buildTVShow(tvShow.Id, req.body);
     tvShows[tvIndex] = tvShow;
-*/
+
     //forma 2
-    tvShows.map(x =>{
+    /*tvShows.map(x =>{
         if(x.Id === parseInt(req.params.id)){
             x = buildTVShow(x.Id, req.body);
             tvShow = x;
             return x;
-
         }else{
             return x;
         }
-    });
+    });*/
     
     res.send(tvShow);
+}
+/**
+ * Eliminar tv show
+ */
+controller.deleteTV = (req, res, next)=>{
+    let tvIndex = tvShows.findIndex(x => x.Id === parseInt(req.params.id));
+    
+    res.send(tvShows.splice(tvIndex, 1));
 }
 
 //---------------------------------------------------------------
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,13 @@ let tvController = controllers.tvController;
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/**
+ * ruteo de tv
+ */
 router.get('/tv', tvController.getTV);
 router.get('/tv/:id', tvController.getTVById);
 router.post('/tv', tvController.addTV);
 router.put('/tv/:id', tvController.updateTV);
+router.delete('/tv/:id', tvController.deleteTV);
 
 module.exports = router;
